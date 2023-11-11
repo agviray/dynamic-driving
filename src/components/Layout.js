0b727c9e433f2a45de3449c720bdc64dd82ca867
Allan Viray <allang.v.dev@gmail.com>
2023-11-10 20:19:03 -0600

    Create sass file structure and apply initial base and reset styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import '../sass/main.scss';
 
 const Layout = ({ children }) => {
-  return (<>
-    <header>
-      <h2>Header Content</h2>
-      <p>The header content will go here</p>
-    </header>
-    <main>{children}</main>
-    <footer>
-      <h2>Footer Content</h2>
-      <p>The footer content will go here.</p>
-    </footer>
-  </>)
+  return (
+    <>
+      <header>
+        <h2>Header Content</h2>
+        <p>The header content will go here</p>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <h2>Footer Content</h2>
+        <p>The footer content will go here.</p>
+      </footer>
+    </>
+  );
 };
 
 export default Layout;
