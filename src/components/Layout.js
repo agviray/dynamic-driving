8acbdb3da2b9a251bfe502bcbb72ebf03574279e
Allan Viray <allang.v.dev@gmail.com>
2023-11-10 18:48:06 -0600

    Create initial Layout component and content

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (<>
+    <header>
+      <h2>Header Content</h2>
+      <p>The header content will go here</p>
+    </header>
+    <main>{children}</main>
+    <footer>
+      <h2>Footer Content</h2>
+      <p>The footer content will go here.</p>
+    </footer>
+  </>)
+};
+
+export default Layout;
