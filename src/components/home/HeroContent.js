fcaffd2379db1bd80e7ca9c0c79a4c4efde25f8b
Allan Viray <allang.v.dev@gmail.com>
2023-11-11 21:01:08 -0600

    Create separate file for hero section of home page content and styles

---
+++ src/components/home/HeroContent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import characters from '../../images/characters.png';
+
+const HeroContent = () => {
+  return (
+    <div className="hero">
+      <div>
+        <h1>
+          <span>DYNAMIC DRIVING</span>
+          <span>DRIVING SCHOOL</span>
+        </h1>
+        <div className="characters">
+          <span>
+            <img src={characters} alt="characters" />
+          </span>
+        </div>
+        <div className="book-button">
+          <span>Let's Drive!</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroContent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import characters from '../images/characters.png';
+import HeroContent from '../components/home/HeroContent';
 
 const Home = () => {
   return (
     <Layout>
       <section>
-        <div className="hero">
-          <div>
-            <h1>
-              <span>DYNAMIC DRIVING</span>
-              <span>DRIVING SCHOOL</span>
-            </h1>
-            <div className="characters">
-              <span>
-                <img src={characters} alt="characters" />
-              </span>
-            </div>
-            <div className="book-button">
-              <span>Let's Drive!</span>
-            </div>
-          </div>
-        </div>
+        <HeroContent />
       </section>
+      <section></section>
     </Layout>
   );
 };
