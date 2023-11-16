e55a5a66c52006c427977dd396c38e7cfac05117
Allan Viray <allang.v.dev@gmail.com>
2023-11-15 21:11:03 -0600

    Update Why content and styles

--- src/components/home/WhyContent.js
+++ src/components/home/WhyContent.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import instructor from '../../images/instructor.png';
+import checklist from '../../images/checklist.png';
+import calendar from '../../images/calendar.png';
 
 const WhyContent = () => {
   return (
@@ -6,45 +9,53 @@ const WhyContent = () => {
       <h2>Why Choose Dynamic Driving</h2>
       <div className="main-content">
         <div className="why-collection">
-          <article>
-            <div className="card">
+          <article className="card">
+            <div>
               <div className="icon-container">
-                <img src="" alt="" />
+                <span className="icon">
+                  <img src={instructor} alt="Instructor icon" />
+                </span>
               </div>
-              <h3>Why 01</h3>
-              <div>
+              <h3>Experienced Instructors</h3>
+              <div className="text-container">
                 <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Porro voluptas dicta quos sapiente hic aliquam, debitis ullam
-                  similique sit. Ducimus?
+                  Our team of certified and experienced instructors brings a
+                  wealth of knowledge and expertise to every lesson, ensuring
+                  you receive the highest quality instruction.
                 </p>
               </div>
             </div>
           </article>
-          <article>
-            <div className="card">
+          <article className="card">
+            <div>
               <div className="icon-container">
-                <img src="" alt="" />
+                <span className="icon">
+                  <img src={checklist} alt="Checklist icon" />
+                </span>
               </div>
-              <h3>Why 02</h3>
-              <div>
+              <h3>Customized Learning Plans</h3>
+              <div className="text-container">
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Doloribus repellendus aliquid vitae!
+                  We recognize that every student is unique. Our personalized
+                  learning plans cater to your individual needs, pace, and
+                  learning style, maximizing your chances of success.
                 </p>
               </div>
             </div>
           </article>
-          <article>
-            <div className="card">
+          <article className="card">
+            <div>
               <div className="icon-container">
-                <img src="" alt="" />
+                <span className="icon">
+                  <img src={calendar} alt="Calendar icon" />
+                </span>
               </div>
-              <h3>Why 03</h3>
-              <div>
+              <h3>Flexible Scheduling</h3>
+              <div className="text-container">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
-                  fugiat at recusandae consequuntur, minus illo.
+                  Life can be busy, and we understand that. That's why we offer
+                  flexible scheduling options to accommodate your commitments
+                  and make it convenient for you to learn at your own pace.
                 </p>
               </div>
             </div>
