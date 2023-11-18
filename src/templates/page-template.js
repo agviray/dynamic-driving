4f28765b936995a9f0209341687569c6fc444aaf
Allan Viray <allang.v.dev@gmail.com>
2023-11-18 13:41:31 -0600

    Create initial page-template content and styles file

---
+++ src/templates/page-template.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PageTemplate = () => {
+  return (
+    <div>
+      <h2>This is a Page Template</h2>
+      <div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio id
+          porro eveniet fugiat nostrum aliquid velit repellat aspernatur dolor
+          provident, tempora minus. Autem tenetur ipsum pariatur asperiores
+          exercitationem delectus voluptates!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PageTemplate;
